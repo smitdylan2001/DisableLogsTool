cb272c77042b48f0d616d34c42378bf79987cb08
Dylan Smit <dylan.smit@hotmail.com>
2021-10-31 16:38:32 +0100

    First setup

---
+++ Assets/Sample/SampleScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScript : MonoBehaviour
+{
+    public void MakeLog()
+    {
+        Debug.Log("This is a log");
+    }
+
+    public void MakeWarning()
+    {
+        Debug.LogWarning("This is a warning");
+    }
+
+    public void MakeError()
+    {
+        Debug.LogError("This is an error");
+    }
+
+    public void ToggleLogs(bool value)
+	{
+        Debug.unityLogger.logEnabled = value;
+    }
+
+    public void ChangeLogType(int value)
+	{
+        DisableLogsTool.ChangeMaxLogType((WhatToLog)value);
+    }
+}
